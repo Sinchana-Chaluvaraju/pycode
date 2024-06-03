46961562e2e82a803b44d3d6d1605ed4917cc11e
Sinchana-Chaluvaraju <113631735+Sinchana-Chaluvaraju@users.noreply.github.com>
2024-06-03 03:45:11 +0200

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,10 @@
+# Hello World
+# Author: Sinchana Chaluvaraju
+# Course: 100 days of Python code
+# Day: 1
+
+
+print("Hello World")
+print(5)
+print("Bye")
+print(17*13)
---
+++ day2.py
@@ -0,0 +1,13 @@
+'''
+Hello World
+Author: Sinchana Chaluvaraju
+Course: 100 days of Python code
+Day: 2
+'''
+print("I am \"Sinchana Chaluvaraju\".\nI am a Masters student at \"TU Darmstadt\"")
+
+
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Sinchana Chaluvaraju")
+
+# sep, end, \"----\" , \n------------->learnt
